30ad2d9ed439ec3b9befcdbbf16914d1d6419544
Lucas Frank Hollmann <lucasfrank_@hotmail.com>
2026-07-06 22:56:45 -0300

    melhoria no layout

--- WindowGroupManager/Views/Controls/WindowDock.xaml.cs
+++ WindowGroupManager/Views/Controls/WindowDock.xaml.cs
@@ -1,18 +1,68 @@
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using WindowGroupManager.Services;
+using WindowGroupManager.Models;
 
 namespace WindowGroupManager.Views.Controls;
 
+public class InvertedBooleanToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool bValue)
+        {
+            return bValue ? Visibility.Collapsed : Visibility.Visible;
+        }
+        return Visibility.Visible;
+    }
 
-public partial class WindowDock : UserControl
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Visibility visibility)
+        {
+            return visibility == Visibility.Visible ? false : true;
+        }
+        return true;
+    }
+}
+
+
+public partial class WindowDock : UserControl, INotifyPropertyChanged
 {
     private IntPtr _windowHandle;
+    private WindowInfo? selectedWindow;
+    private bool _isEmpty = true; // Por padrão, vazio (mostra StackPanel)
+    private readonly WindowManager _windowManager =
+        new();
 
+    public bool IsEmpty
+    {
+        get => _isEmpty;
+        set
+        {
+            if (_isEmpty != value)
+            {
+                _isEmpty = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
     public WindowDock()
     {
         InitializeComponent();
+        DataContext = this;
 
         SizeChanged +=
             WindowDock_SizeChanged;
@@ -31,14 +81,13 @@ public partial class WindowDock : UserControl
     public void AttachWindow(IntPtr handle)
     {
         _windowHandle = handle;
-
+        IsEmpty = false;
 
         if (!Host.IsInitialized)
         {
             Host.Initialized += Host_Initialized;
             return;
         }
-
 
         AttachInternal();
     }
@@ -97,5 +146,91 @@ public partial class WindowDock : UserControl
             (int)ActualWidth,
             (int)ActualHeight,
             true);
+    }
+
+    private void RefreshButton_Click(
+        object sender,
+        RoutedEventArgs e)
+    {
+        LoadWindows();
+    }
+
+    private void AttachWindowButton_Click(
+        object sender,
+        RoutedEventArgs e)
+    {
+        // Open a small popup window (dropdown-like) under the Attach button
+        var owner = Window.GetWindow(this);
+
+        var popup = new Window
+        {
+            Owner = owner,
+            WindowStyle = WindowStyle.ToolWindow,
+            ShowInTaskbar = false,
+            ResizeMode = ResizeMode.NoResize,
+            Width = 320,
+            Height = 300,
+            Topmost = true
+        };
+
+        // Build list of windows
+        var list = new ListBox();
+        list.DisplayMemberPath = "ToString";
+        list.ItemsSource = _windowManager.GetWindows();
+        list.MouseDoubleClick += (s, ev) =>
+        {
+            if (list.SelectedItem is WindowInfo win)
+            {
+                selectedWindow = win;
+                popup.Close();
+                AttachWindow(win.Handle);
+            }
+        };
+
+        // Optional select button
+        var selectButton = new Button { Content = "Select", Margin = new Thickness(4), HorizontalAlignment = HorizontalAlignment.Right };
+        selectButton.Click += (s, ev) =>
+        {
+            if (list.SelectedItem is WindowInfo win)
+            {
+                selectedWindow = win;
+                popup.Close();
+                AttachWindow(win.Handle);
+            }
+        };
+
+        var panel = new DockPanel();
+        DockPanel.SetDock(selectButton, Dock.Bottom);
+        panel.Children.Add(selectButton);
+        panel.Children.Add(list);
+
+        popup.Content = panel;
+
+        // Position popup under the Attach button if possible
+        if (sender is FrameworkElement fe)
+        {
+            var pt = fe.PointToScreen(new System.Windows.Point(0, fe.ActualHeight));
+            // Convert screen to owner window coordinates
+            var presentationSource = System.Windows.PresentationSource.FromVisual(owner);
+            if (presentationSource != null)
+            {
+                var transform = presentationSource.CompositionTarget.TransformFromDevice;
+                var target = transform.Transform(pt);
+                popup.Left = target.X;
+                popup.Top = target.Y;
+            }
+            else
+            {
+                popup.Left = pt.X;
+                popup.Top = pt.Y;
+            }
+        }
+
+        popup.Show();
+    }
+
+    private void LoadWindows()
+    {
+        //ItemsSource = _windowManager.GetWindows();
     }
 }
--- WindowGroupManager/Views/MainWindow.xaml.cs
+++ WindowGroupManager/Views/MainWindow.xaml.cs
@@ -6,34 +6,12 @@ namespace WindowGroupManager.Views;
 
 public partial class MainWindow : Window
 {
-    private WindowInfo? selectedWindow;
-    private readonly WindowManager _windowManager =
-        new();
-
     public MainWindow()
     {
         InitializeComponent();
-
-        WindowsList.SelectionChanged +=
-            WindowsList_SelectionChanged;
-
-        LoadWindows();
     }
 
-    private void RefreshButton_Click(
-        object sender,
-        RoutedEventArgs e)
-    {
-        LoadWindows();
-    }
-
-    private void LoadWindows()
-    {
-        WindowsList.ItemsSource =
-            _windowManager.GetWindows();
-    }
-
-    private void WindowsList_SelectionChanged(
+    /*private void WindowsList_SelectionChanged(
         object sender,
         SelectionChangedEventArgs e)
     {
@@ -49,29 +27,5 @@ public partial class MainWindow : Window
             $"HWND: 0x{selectedWindow.Handle.ToInt64():X}\n" +
             $"PID: {selectedWindow.ProcessId}\n" +
             $"Processo: {selectedWindow.ProcessName}";
-    }
-
-    private void AttachLeftButton_Click(
-        object sender,
-        RoutedEventArgs e)
-    {
-        if (selectedWindow == null)
-            return;
-
-
-        LeftDock.AttachWindow(
-            selectedWindow.Handle);
-    }
-
-    private void AttachRightButton_Click(
-        object sender,
-        RoutedEventArgs e)
-    {
-        if (selectedWindow == null)
-            return;
-
-
-        RightDock.AttachWindow(
-            selectedWindow.Handle);
-    }
+    }*/
 }
